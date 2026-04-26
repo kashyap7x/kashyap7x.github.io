8beaa52b26b50ab77a735937b4d267fdd10c9c80
Kashyap Chitta <kashyap.chitta@gmail.com>
2026-04-26 20:51:32 +0200

    Update bio text

--- site_config.py
+++ site_config.py
@@ -34,7 +34,7 @@ def get_personal_data():
         "site_title_suffix": site_title_suffix,
         "description": f"{full_name} is an AI researcher working on {research_focus}.",
         "publications_description": f"Complete publication list for {full_name}, including papers on {research_topics}.",
-        "talks_description": f"Selected invited talks, tutorials, and seminar presentations by {full_name} on {research_topics}.",
+        "talks_description": f"Selected invited talks by {full_name} on {research_topics}.",
         "location": location,
         "research_focus": research_focus,
         "profile_image": profile_image,
@@ -48,11 +48,12 @@ def get_personal_data():
     }
 
     bio_details = """
-                    Kashyap did a bachelor's degree in electronics at the <a href="https://www.rvce.edu.in/" target="_blank">RV College of Engineering</a>, India. He then moved to the US in 2017 to obtain his Master's degree in computer vision from <a href="https://www.ri.cmu.edu/" target="_blank">Carnegie Mellon University</a>, where he was advised by <a href = "http://www.cs.cmu.edu/~hebert/" target="_blank">Prof. Martial Hebert</a>. During this time, he was also an intern at the <a href = "https://research.nvidia.com/labs/av-applied-research/" target="_blank">NVIDIA Autonomous Vehicles Applied Research Group</a> working with <a href = "https://alvarezlopezjosem.github.io/" target="_blank">Dr. Jose M. Alvarez</a>. From 2019, he was a PhD student in the <a href="https://uni-tuebingen.de/en/fakultaeten/mathematisch-naturwissenschaftliche-fakultaet/fachbereiche/informatik/lehrstuehle/autonomous-vision/home/" target="_blank">Autonomous Vision Group</a> at the University of Tübingen, Germany, supervised by <a href="http://cvlibs.net/" target="_blank">Prof. Andreas Geiger</a>. He was selected for the <a href="https://iccv2023.thecvf.com/doctoral.consortium-353000-2-30.php" target="_blank">doctoral consortium</a> at ICCV 2023, as a 2023 <a href="https://sites.google.com/view/rsspioneers2023/participants" target="_blank">RSS pioneer</a>, and an outstanding reviewer for <a href="https://cvpr2023.thecvf.com/Conferences/2023/OutstandingReviewers" target="_blank">CVPR</a>, <a href="https://twitter.com/kashyap7x/status/1712169445349560517" target="_blank">ICCV</a>, <a href="https://eccv.ecva.net/Conferences/2024/Reviewers" target="_blank">ECCV</a>, and <a href="https://neurips.cc/Conferences/2023/ProgramCommittee#top-reivewers" target="_blank">NeurIPS</a>. He has also won multiple autonomous driving challenge awards <a href="https://opendrivelab.com/challenge2023/#nuplan_planning" target="_blank">[nuPlan 2023]</a> <a href="https://leaderboard.carla.org/challenge/#previous-carla-ad-challenges" target="_blank">[CARLA 2020, 2021, 2022, 2023, 2024]</a> <a href="https://waymo.com/open/challenges/" target="_blank">[Waymo 2025]</a> <a href="https://realadsim.github.io/2025/#challenge" target="_blank">[HUGSIM 2025]</a>.
+                    <p>I earned my bachelor's degree in electronics from the <a href="https://www.rvce.edu.in/" target="_blank">RV College of Engineering</a>, India, before moving to the US in 2017 for a Master's degree in computer vision at <a href="https://www.ri.cmu.edu/" target="_blank">Carnegie Mellon University</a>, where I was advised by <a href = "http://www.cs.cmu.edu/~hebert/" target="_blank">Prof. Martial Hebert</a>. During this time, I was also an intern at the <a href = "https://research.nvidia.com/labs/av-applied-research/" target="_blank">NVIDIA Autonomous Vehicles Applied Research Group</a>, working with <a href = "https://alvarezlopezjosem.github.io/" target="_blank">Dr. Jose M. Alvarez</a>.</p>
+                    <p>From 2019, I was a PhD student in the <a href="https://uni-tuebingen.de/en/fakultaeten/mathematisch-naturwissenschaftliche-fakultaet/fachbereiche/informatik/lehrstuehle/autonomous-vision/home/" target="_blank">Autonomous Vision Group</a> at the University of Tübingen, Germany, supervised by <a href="http://cvlibs.net/" target="_blank">Prof. Andreas Geiger</a>. I was selected for the <a href="https://iccv2023.thecvf.com/doctoral.consortium-353000-2-30.php" target="_blank">doctoral consortium</a> at ICCV 2023, named a 2023 <a href="https://sites.google.com/view/rsspioneers2023/participants" target="_blank">RSS Pioneer</a>, and recognized as an outstanding reviewer for <a href="https://cvpr2023.thecvf.com/Conferences/2023/OutstandingReviewers" target="_blank">CVPR</a>, <a href="https://twitter.com/kashyap7x/status/1712169445349560517" target="_blank">ICCV</a>, <a href="https://eccv.ecva.net/Conferences/2024/Reviewers" target="_blank">ECCV</a>, and <a href="https://neurips.cc/Conferences/2023/ProgramCommittee#top-reivewers" target="_blank">NeurIPS</a>. I have also won multiple autonomous driving challenge awards <a href="https://opendrivelab.com/challenge2023/#nuplan_planning" target="_blank">[nuPlan 2023]</a> <a href="https://leaderboard.carla.org/challenge/#previous-carla-ad-challenges" target="_blank">[CARLA 2020, 2021, 2022, 2023, 2024]</a> <a href="https://waymo.com/open/challenges/" target="_blank">[Waymo 2025]</a> <a href="https://realadsim.github.io/2025/#challenge" target="_blank">[HUGSIM 2025]</a>.</p>
     """
     bio_text = f"""
                 <p>
-                    I am a {personal_data['job_title']} at the <a href="{personal_data['organization']['url']}" target="_blank">{personal_data['organization']['name']}</a> working from {personal_data['location']}. My research focuses on {personal_data['research_focus']}.
+                    I am a {personal_data['job_title']} at the <a href="{personal_data['organization']['url']}" target="_blank">{personal_data['organization']['name']}</a>, based in {personal_data['location']}. My research focuses on {personal_data['research_focus']}.
                 </p>
                 <div class="social-links">
                     <details class="bio-dropdown">
@@ -66,7 +67,7 @@ def get_personal_data():
                     <a href="{personal_data['profiles']['github']}" target="_blank" class="social-link"><i class="fab fa-github fa-lg"></i> GitHub</a>
                     <a href="{personal_data['profiles']['youtube']}" target="_blank" class="social-link"><i class="fab fa-youtube fa-lg"></i> YouTube</a>
                     <div class="bio-dropdown-content">
-                        <p>{bio_details}</p>
+                        {bio_details}
                     </div>
                 </div>
     """
