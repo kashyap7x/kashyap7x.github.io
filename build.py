00e4d85990f7e1e21671ecc12f00859012fa23c6
Kashyap Chitta <kashyap.chitta@gmail.com>
2026-04-26 19:19:18 +0200

    Add Cloudflare web analytics

--- build.py
+++ build.py
@@ -457,6 +457,9 @@ def get_html_footer():
         });
       });
     </script>
+    <!-- Cloudflare Web Analytics -->
+    <script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "556cab14f28a44af930eb08b42be4126"}'></script>
+    <!-- End Cloudflare Web Analytics -->
 </body>
 
 </html>
